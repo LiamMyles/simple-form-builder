4a2f63973c95ec1a56b97271d8fd312da7c8876d
Liam Myles <liamthemonkey92@gmail.com>
2020-09-01 02:02:37 +0100

    Updates

---
+++ precache-manifest.0a9b53f886f9b9cbf0dff6f110a888ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3abb76b86132655f2047486fb3100f19",
+    "url": "/simple-form-builder/index.html"
+  },
+  {
+    "revision": "d887e86f09f43d72c9f2",
+    "url": "/simple-form-builder/static/css/main.f6af1bd5.chunk.css"
+  },
+  {
+    "revision": "8e5f3f4c571bb54418cf",
+    "url": "/simple-form-builder/static/js/2.90bc1bd1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-form-builder/static/js/2.90bc1bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d887e86f09f43d72c9f2",
+    "url": "/simple-form-builder/static/js/main.53f8bfcc.chunk.js"
+  },
+  {
+    "revision": "976bd6f7590fe0043d54",
+    "url": "/simple-form-builder/static/js/runtime-main.b981fdd0.js"
+  }
+]);
